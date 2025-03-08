5b5dd68a561e933aaae4defcbb736805d8ac80ef
JaideepSingh99 <jai46560@gmail.com>
2025-03-08 19:30:45 +0530

    Add user authorization

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 
-
+require('dotenv').config();
 const app = express();
 const port = 8000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: "http://localhost:5173"}));
+
+mongoose.connect(process.env.MONGO_URI);
+
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
 
 app.get('/', (req, res) => {
     res.json("Server is Runnig");
